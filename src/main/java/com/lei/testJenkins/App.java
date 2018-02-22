88445d1955e6be5e27bb6af54fa7ba9f3e9ca09c
Lei <classloader@gmail.com>
2018-02-22 20:36:27 +0000

    init commit

---
+++ src/main/java/com/lei/testJenkins/App.java
@@ -0,0 +1,9 @@
+package com.lei.testJenkins;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("()&*(##^@#$%#$^&*( ---- Hello GitHub! ----  #@#^&(*_)(@#%&^%$#&%&*" );
+    }
+}
